c871f030703ff08e3f3759523d5a7ed03bf63b11
jmisrikhanov <jmisrikhanov@gmail.com>
2021-09-26 21:27:44 +0200

    Day 1

---
+++ HackerRank/10 Days of Javascript/Day 1 'Arithmetic Operators', 'Functions', 'Let and Const'/3.question.js
@@ -0,0 +1,7 @@
+// readLine means that whatever the user's input is, it is gonna take its value
+
+function main() {
+  // Write your code here. Read input using 'readLine()' and print output using 'console.log()'.
+  // Print the area of the circle:
+  // Print the perimeter of the circle:
+}
