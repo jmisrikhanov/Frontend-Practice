a2bee24f4680a9c321dcbcfc8af8ee54a6c99310
jmisrikhanov <jmisrikhanov@gmail.com>
2021-12-23 15:06:52 +0100

    new solution

--- HackerRank/10 Days of Javascript/Day 3 'Arrays' 'Try, catch and finally' 'Throw'/2.solution.js
+++ HackerRank/10 Days of Javascript/Day 3 'Arrays' 'Try, catch and finally' 'Throw'/2.solution.js
@@ -9,3 +9,14 @@ function reverseString(s) {
     console.log("s.split is not a function" + "\n" + s);
   }
 }
+
+// 2nd solution
+function reverseString(s) {
+  try {
+    s = s.split("").reverse().join("");
+  } catch (e) {
+    console.log("s.split is not a function");
+  }
+
+  console.log(s);
+}
