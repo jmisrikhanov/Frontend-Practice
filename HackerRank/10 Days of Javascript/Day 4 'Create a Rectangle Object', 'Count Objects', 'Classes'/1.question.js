15ed5863df0594d2078a15c02ee15ad35cb09d3f
jmisrikhanov <jmisrikhanov@gmail.com>
2021-12-23 15:32:24 +0100

    task 1 question and solution

---
+++ HackerRank/10 Days of Javascript/Day 4 'Create a Rectangle Object', 'Count Objects', 'Classes'/1.question.js
@@ -0,0 +1,20 @@
+// Task
+
+// Complete the function in the editor. It has two parameters: a and b.
+// It must return an object modeling a rectangle that has the following properties:
+
+//  length: This value is equal to a.
+
+//  width: This value is equal to b.
+
+//  perimeter: This value is equal to 2 - (a + b)
+
+//  area: This value is equal to a - b
+
+// Note: The names of the object's properties must be spelled correctly to pass this
+// chaHenge.
+
+/*
+ * Complete the Rectangle function
+ */
+function Rectangle(a, b) {}
