5a4de2df0a5a992b3adc5270080d81ec6b7ae8a3
jmisrikhanov <jmisrikhanov@gmail.com>
2021-12-26 00:09:48 +0100

    task 3 question and solution

---
+++ HackerRank/10 Days of Javascript/Day 5 'Inheritance', 'Template Literals', 'Arrow Functions'/3.question.js
@@ -0,0 +1,7 @@
+/*
+ * Modify and return the array so that all even elements are doubled and all odd elements are tripled.
+ *
+ * Parameter(s):
+ * nums: An array of numbers.
+ */
+function modifyArray(nums) {}
