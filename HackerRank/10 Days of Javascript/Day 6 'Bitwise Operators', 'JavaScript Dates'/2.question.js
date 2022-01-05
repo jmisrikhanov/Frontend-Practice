49d5ab3cef3dbd386affc529fd93e80899ee81c1
jmisrikhanov <jmisrikhanov@gmail.com>
2022-01-05 23:13:15 +0100

    task 2 question and solution

---
+++ HackerRank/10 Days of Javascript/Day 6 'Bitwise Operators', 'JavaScript Dates'/2.question.js
@@ -0,0 +1,7 @@
+// The days of the week are: "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+function getDayName(dateString) {
+    let dayName;
+    // Write your code here
+    
+    return dayName;
+}
