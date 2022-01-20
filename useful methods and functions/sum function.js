5812f1f87686d0ecba614d0dcf0de588c0e82436
jmisrikhanov <jmisrikhanov@gmail.com>
2022-01-20 15:30:10 +0100

    sum function

---
+++ useful methods and functions/sum function.js
@@ -0,0 +1,36 @@
+// in this examples args show the rest arguments apart from a, b. However, arguments show all arguments
+
+function sum(a, b, ...args) {
+  console.log(arguments);
+  let sum = 0;
+  for (let i = 0; i < arguments.length; i++) {
+    sum += arguments[i];
+  }
+  return sum;
+}
+
+console.log(sum(1, 2, 3, 100));
+
+// same as above
+function sum(...args) {
+  let sum = 0;
+  for (let i = 0; i < arguments.length; i++) {
+    sum += arguments[i];
+  }
+  return sum;
+}
+
+console.log(sum(1, 2, 3, 100));
+
+// with reduce
+function sum(...theArgs) {
+  return theArgs.reduce((previous, current) => {
+    return previous + current;
+  });
+}
+
+console.log(sum(1, 2, 3));
+// expected output: 6
+
+console.log(sum(1, 2, 3, 4));
+// expected output: 10
