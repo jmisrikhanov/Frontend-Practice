5bb87492b70188fec1dfd1eb18b3ff0d4cb66529
jmisrikhanov <jmisrikhanov@gmail.com>
2022-03-12 21:49:46 +0100

    console.table()

---
+++ useful methods and functions/console.table().js
@@ -0,0 +1,8 @@
+// Easy way to get a quick overview of an array of objects
+
+const people = [
+  { name: Jake, age: 12 },
+  { name: Dirk, age: 19 },
+  { name: Wayne, age: 27 },
+];
+console.table(people);
