9f6c97cfc525ef9080787a69e8574ba4d6f50760
jmisrikhanov <jmisrikhanov@gmail.com>
2022-03-17 20:50:27 +0100

    Copy text using navigator.clipboard.writeText

--- useful methods and functions/Copy to Clipboard.js
+++ useful methods and functions/Copy to Clipboard.js
@@ -1,5 +1,7 @@
 // Copy to Clipboard
 
+// Easily copy any text to clipboard using navigator.clipboard.writeText
+
 const copyToClipboard = (text) => navigator.clipboard.writeText(text);
 
 copyToClipboard("Hello World");
