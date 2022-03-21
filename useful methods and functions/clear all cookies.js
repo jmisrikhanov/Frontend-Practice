e37b4b6c4ee946cdfb3001515ce9f4beb0dd5625
jmisrikhanov <jmisrikhanov@gmail.com>
2022-03-21 15:50:34 +0100

    clear all cookies

---
+++ useful methods and functions/clear all cookies.js
@@ -0,0 +1,10 @@
+// clear all cookies
+
+const clearCookies = document.cookie
+  .split(";")
+  .forEach(
+    (cookie) =>
+      (document.cookie = cookie
+        .replace(/^ +/, "")
+        .replace(/=.*/, `=;expires=${new Date(0).toUTCString()};path=/`))
+  );
