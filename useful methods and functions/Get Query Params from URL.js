c95df957700d02df074a46cafa7a2abd35b2ae1b
jmisrikhanov <jmisrikhanov@gmail.com>
2022-03-26 05:05:32 +0100

    Get Query Params from URL

---
+++ useful methods and functions/Get Query Params from URL.js
@@ -0,0 +1,14 @@
+// Get Query Params from URL
+
+const getParameters = (URL) => {
+  URL = JSON.parse(
+    '{"' +
+      decodeURI(URL.split("?")[1])
+        .replace(/"/g, '\\"')
+        .replace(/&/g, '","')
+        .replace(/=/g, '":"') +
+      '"}'
+  );
+  return JSON.stringify(URL);
+};
+getParameters(window.location);
