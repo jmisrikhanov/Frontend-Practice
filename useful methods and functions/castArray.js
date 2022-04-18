e40bd820ec1ae584a2131b70c46f077d6b8e2fb8
jmisrikhanov <jmisrikhanov@gmail.com>
2022-04-18 21:10:15 +0200

    converting a non-array value into array

---
+++ useful methods and functions/castArray.js
@@ -0,0 +1,8 @@
+// castArray
+
+// This snippet converts a non-array value into array.
+
+const castArray = (val) => (Array.isArray(val) ? val : [val]);
+
+castArray("foo"); // ['foo']
+castArray([1]); // [1]
