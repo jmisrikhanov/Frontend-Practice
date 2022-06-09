511b94a59248caed52f830248d0d63c16e619c3f
jmisrikhanov <jmisrikhanov@gmail.com>
2022-06-09 20:27:46 +0200

    solution 2 with for loop

---
+++ leetcode/20. Valid Parentheses/solution2 - for.js
@@ -0,0 +1,16 @@
+var isValid = function (s) {
+  const hashMap = { "(": ")", "{": "}", "[": "]" };
+  let heap = [];
+  for (let char of s) {
+    if (hashMap[char]) {
+      heap.push(hashMap[char]);
+    } else {
+      if (heap.pop() !== char) return false;
+    }
+  }
+
+  return !heap.length;
+};
+
+// Runtime: 73 ms, faster than 69.41% of JavaScript online submissions for Valid Parentheses.
+// Memory Usage: 42.2 MB, less than 66.63% of JavaScript online submissions for Valid Parentheses.
