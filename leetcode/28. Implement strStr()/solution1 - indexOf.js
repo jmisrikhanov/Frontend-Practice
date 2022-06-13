e9e95ee2264de10080ba93df8b2eff67b0681d4e
jmisrikhanov <jmisrikhanov@gmail.com>
2022-06-13 18:12:40 +0200

    solution 1 with indexOf

---
+++ leetcode/28. Implement strStr()/solution1 - indexOf.js
@@ -0,0 +1,6 @@
+var strStr = function (haystack, needle) {
+  return haystack.indexOf(needle);
+};
+
+// Runtime: 65 ms, faster than 83.36% of JavaScript online submissions for Implement strStr().
+// Memory Usage: 41.4 MB, less than 98.53% of JavaScript online submissions for Implement strStr().
